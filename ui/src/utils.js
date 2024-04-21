d3e8b3f717edffb384b7bf40768669e55eea832e
Mourin Gayed <morinadel11@gmail.com>
2024-04-20 22:40:25 -0400

    feat: updated api url

--- ui/src/utils.js
+++ ui/src/utils.js
@@ -1,2 +1,3 @@
 //utils.js
-export const API_URL = "localhost:3001/task";
+//export const API_URL = "localhost:3001/task";
+export const API_URL = "https://wv746pxqluaw4vqnajxvrdgrkq0rdpwi.lambda-url.us-east-2.on.aws/task";
